257708b0657efa9a669f29dda13e5776fb2dbf63
liu-miao-miao <446740390@qq.com>
2019-09-17 18:42:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,3 @@ export default function openAppStoreToComment (appId = 1210643082) {
     RNMarketCommentModule.show();
   }
 }
-
-export default function onKeyDown (isLister = false) {
-  if (Platform.OS === 'android') {
-    RNMarketCommentModule.onKeyDown(isLister);
-  }
-}
